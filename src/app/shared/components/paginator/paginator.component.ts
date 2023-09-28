cae9c0e984794475b4e4a1d7d6c167201f88db74
Jramos <johnatan.r1000@gmail.com>
2023-09-27 22:17:49 -0500

    Se agrega el componente para paginar.

---
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+
+import { MaterialModule } from '../../modules/material.module';
+import { BaseClass } from 'src/app/core/base.class';
+import { IFilters } from 'src/app/store/reducers/filters.reducer';
+import * as actions from '../../../store/actions/filters.actions';
+
+@Component({
+  selector: 'app-paginator',
+  standalone: true,
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './paginator.component.html',
+  styleUrls: ['./paginator.component.scss']
+})
+export class PaginatorComponent extends BaseClass implements OnInit {
+  @Input() totalRecords!: number;
+  
+  $store!: Subscription;
+  page!: number;
+
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.$store = this.store.select('filters').subscribe({
+      next: (res: IFilters) => {
+        this.page = res.page || 0;
+      }
+    });
+  }
+
+  changePage(event: PageEvent) {
+    this.store.dispatch(actions.setFilters({ page: event.pageIndex, limit: event.pageSize }));
+  }
+}
