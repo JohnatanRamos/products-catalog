f533e680e92b002142f44112e4d784d2165f4e2a
Jramos <johnatan.r1000@gmail.com>
2023-09-29 11:21:30 -0500

    Se agrega el pipe de currency para que quede en pesos colombianos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import localeEs from "@angular/common/locales/es-CO";
+import { registerLocaleData } from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputFilterComponent } from './shared/components/input-filter/input-filter.component';
 import { PriceFilterComponent } from './shared/components/price-filter/price-filter.component';
 import { appReducers } from './store/app.reducer';
+import { MaterialModule } from './shared/modules/material.module';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -22,10 +27,11 @@ import { appReducers } from './store/app.reducer';
     HttpClientModule,
     InputFilterComponent,
     PriceFilterComponent,
+    MaterialModule,
     StoreModule.forRoot(appReducers),
     SweetAlert2Module.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: "es-CO" }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
